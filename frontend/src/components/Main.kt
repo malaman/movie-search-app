e4145c8b53dc0d45d328ec98a7e19fed8ca1786f
Andrii Malaman <and.malaman@gmail.com>
2018-03-27 12:46:44 +0200

    - add wrapper around react-select

--- frontend/src/components/Main.kt
+++ frontend/src/components/Main.kt
@@ -16,12 +16,39 @@ class Main : RComponent<RProps, RState>() {
         }
         axios<SearchResponse>(config).then { response ->
             println(response.data.results[0].Title)
-        }
+        }              
     }
+
+    fun onSelectChange(option: Option) {
+        println(option)
+    }
+
+    fun onInputValueChange(value: String) {
+        println(value)
+    }
+
+    val options : Array<Option> = arrayOf(
+        jsObject {
+            value = "1"
+            label = "1"
+        },
+        jsObject {
+            value = "2"
+            label = "2"
+        }        
+    )  
+
 
     override fun RBuilder.render() {
         div {
             "This is custom component with xhr request"
+            customSelect(
+                name="12313",
+                value="1",
+                options=options,
+                onChange={opt: Option -> onSelectChange(opt)},
+                onInputChange = {value: String -> onInputValueChange(value)}                
+            )
         }
     }
 }
---
+++ frontend/src/components/Select.kt
@@ -0,0 +1,59 @@
+package components
+
+import react.*
+import react.dom.*
+
+@JsModule("react-select/dist/react-select")
+external val reactSelect: RClass<SelectProps>
+
+external interface Option {
+    var value: String
+    var label: String
+}
+
+external interface SelectProps : RProps {
+    var name: String
+    var options: Array<Option>
+    var value: String
+    var onInputChange: (String) -> Unit
+    var onChange: (Option) -> Unit
+    var isLoading: Boolean
+    var backspaceRemoves: Boolean
+}
+
+class CustomSelect(props: SelectProps) : RComponent<SelectProps, RState>(props) {
+
+    override fun RBuilder.render() {        
+        div {            
+            reactSelect {
+                attrs {
+                    name=props.name
+                    options=props.options
+                    value=props.value
+                    onInputChange=props.onInputChange
+                    onChange=props.onChange
+                    isLoading=props.isLoading
+                    backspaceRemoves=props.backspaceRemoves
+                }
+            }                    
+        }
+    }
+}
+
+fun RBuilder.customSelect(
+    name: String,
+    options: Array<Option>,
+    value: String,
+    onInputChange: (String) -> Unit, 
+    onChange: (Option) -> Unit,
+    isLoading: Boolean = false,
+    backspaceRemoves: Boolean = true    
+) = child(CustomSelect::class) {
+                attrs.name=name
+                attrs.options=options
+                attrs.value=value
+                attrs.onInputChange=onInputChange
+                attrs.onChange=onChange
+                attrs.isLoading=isLoading
+                attrs.backspaceRemoves=backspaceRemoves    
+}
